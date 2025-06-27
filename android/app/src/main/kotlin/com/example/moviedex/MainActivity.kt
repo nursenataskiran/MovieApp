cd128c14187c9a36aed9166f86ac976892d5df42
nt <nursenataskiran@hotmail.com>
2025-06-27 21:36:22 +0300

    Revert "android"
    This reverts commit 8e4c510924fae856fcbf0c13b7743b1583f873e6.

--- android/app/src/main/kotlin/com/example/moviedex/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviedex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/kodify/moviedex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kodify.moviedex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
